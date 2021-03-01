bd087bfb3a386aa0e4e355a07f6eb080c3a404f0
mmmG <a331998513@gmail.com>
2021-03-01 06:41:37 +0100

    1

--- docs/quotemachine.js
+++ docs/quotemachine.js
@@ -1,6 +1,6 @@
 const link = 'https://api.quotable.io/random'   // random quote
-
 //adapt random color
+
 const randomcolors = () =>{
     const colors = ['#203A43', '#c471ed', '#1565C0', '#4286f4', '#493240', '#aa4b6b', '#3b8d99']; // color collection
     const randomize = Math.floor(Math.random() * colors.length);  // random number
@@ -16,15 +16,25 @@ const randomcolors = () =>{
     // }
     texts.setAttribute('style', 'color: ${randomizer}');
 }
+const text = document.querySelector('#text');
+const author = document.querySelector('#author');
+const button = document.querySelector('#button-box')
 
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector('button')
-})
-async (getQuote) => {
-    
+
+
+async function getQuote() {
     const response = await fetch(link);
+   
     const data = await response.json();
         if (response.ok) {
-
+            text.textContent = data.content;
+            author.textContent = data.author;
+        } else {
+            text.textContent = data.content;
+            console.log(data)
         }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    getQuote();
+  }
