454d2f6839f5fbee41c3a4177e05087b98176c39
mmmG <a331998513@gmail.com>
2021-03-01 02:12:58 +0100

    change

---
+++ docs/quotemachine.js
@@ -0,0 +1,23 @@
+const link = 'https://api.quotable.io/random'   // random quote
+
+//adapt random color
+const randomcolors = () =>{
+    const colors = ['#203A43', '#c471ed', '#1565C0', '#4286f4', '#493240', '#aa4b6b', '#3b8d99']; // color collection
+    const randomize = Math.floor(Math.random() * colors.length);  // random number
+    const randomizer = colors[randomize]; // randomize color's array number
+
+    //DOM
+    const bgcolor = document.querySelector('.random-bgcolor ');
+    bgcolor.setAttribute('style','background-color: ${randomizer}'); //set randomized bgcolor
+
+    const texts = document.querySelectorAll('.random-txtcolor');
+    // for(const text of texts){
+    //     text.setAttribute('style', 'color: ${randomizer}')
+    // }
+    texts.setAttribute('style', 'color: ${randomizer}');
+}
+
+// get Quote
+const Quote = (data) => {
+    const callback = 'displayQuote'
+}
