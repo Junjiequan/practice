26f59e3820915bec0c81eb7a7c2dae57e58fa73e
mmmG <a331998513@gmail.com>
2021-03-02 00:49:22 +0100

    95%done

--- docs/quote/quotemachine.js
+++ docs/quote/quotemachine.js
@@ -1,4 +1,18 @@
 const API = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+const colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
 
 class APP extends React.Component{
     state = {
@@ -8,8 +22,10 @@ class APP extends React.Component{
                 author: "randome guy"
             }
         ],
-        index:0
+        index:0,
+        bgColor: '#BDBB99'
     }
+
     componentDidMount=()=>{
         fetch(API)
         .then(result => result.json())
@@ -19,7 +35,7 @@ class APP extends React.Component{
             })
         });
     }
-    getRandomIndex =()=>{
+getRandomIndex = () =>{
         const {quotes} = this.state;
         
         if(quotes.length > 0){
@@ -29,40 +45,51 @@ class APP extends React.Component{
             })
         }
     }
+    componentDidUpdate(){
+        const {bgColor} = this.state;
+        const bodybg = document.querySelector("body");
+        bodybg.style.backgroundColor = bgColor;
+        const btnbg = document.querySelector("button");
+        btnbg.style.backgroundColor = bgColor;
+        const abg = document.querySelector("a");
+        abg.style.backgroundColor = bgColor;
+        const pcolor = document.querySelector("p");
+        pcolor.style.color = bgColor;
+        const citecolor = document.querySelector("cite");
+        citecolor.style.color = bgColor;
+}  
+getRandomColor = () =>{
+    const bgColor = this.state;
+    if(colors.length > 0){
+        const bgColor = colors[Math.floor(Math.random() * colors.length)];
+        this.setState({
+            bgColor
+        })
+    }
+}
+ wrapperFunction = () => {
+        this.getRandomIndex();
+        this.getRandomColor();
+     }
     render(){
-        const colors = [
-            '#16a085',
-            '#27ae60',
-            '#2c3e50',
-            '#f39c12',
-            '#e74c3c',
-            '#9b59b6',
-            '#FB6964',
-            '#342224',
-            '#472E32',
-            '#BDBB99',
-            '#77B1A9',
-            '#73A857'
-          ];
+       
         const article = this.state.quotes[this.state.index];
         const tweetlink = `https://twitter.com/intent/tweet?text=${article.quote} - ${article.author}`;
-        const facebook = 'https://www.youtube.com/watch?v=iGWei_0EJIc'
-        
+        const facebook = 'https://www.youtube.com/watch?v=iGWei_0EJIc';
         return (
             <div className="d-flex justify-content-center vh-100 align-items-center">
-                <div className="col-6 p-5 box rounded quote-box">
+                <div className="col-6 p-4 box rounded quote-box">
                         {article && (
                             <div id="boxSize" >
-                                <p>{article.quote}</p>
+                                <p><span className="fs-2 me-2"><i className="fa fa-quote-left"></i></span>{article.quote}</p>
                                 <cite id="author">- {article.author}</cite>
                             </div>
                         )}
                         <div className="d-flex justify-content-between">
                             <div >
-                            <a className="btn btn-primary " href={tweetlink} target="_blank"><i className="fab fa-twitter"></i></a>
-                            <a className="btn btn-primary ms-1" href={facebook} target="_blank"><i className="fab fa-youtube"></i></a>
+                              <a className="btn btn-primary " href={tweetlink} target="_blank"><i className="fab fa-twitter"></i></a>
                             </div>
-                            <button className="btn btn-primary" onClick={this.getRandomIndex.bind(this)}>Get Quote</button>
+                            <button className="btn btn-primary" onClick={this.wrapperFunction}>Get Quote</button>
                         </div>
                 </div>
             </div>
@@ -70,8 +97,4 @@ class APP extends React.Component{
     }
 
 }
-
-
-
-
 ReactDOM.render(<APP />, document.getElementById('app'))
