daf342ae5d89295de14941fcbe4f3681f566c26c
mmmG <a331998513@gmail.com>
2021-03-04 00:45:15 +0100

    uploade

---
+++ docs/drum_machine/drum_machine.js
@@ -0,0 +1,22 @@
+class App extends React.Component{
+        constructor(props){
+            super(props);
+
+            this.state={
+
+            }
+        }
+
+
+        render(){
+
+            return (
+                <div class="button"></div>
+                <div class="control"></div>
+            )
+        }
+
+
+
+
+}
