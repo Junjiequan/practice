78d16429051bcf35168eb135a9a49caf9c49e326
mmmG <a331998513@gmail.com>
2021-03-04 05:25:30 +0100

    50%

--- docs/drum_machine/drum_machine.js
+++ docs/drum_machine/drum_machine.js
@@ -1,50 +1,103 @@
-class App extends React.Component{
-        constructor(props){
-            super(props);
+const audioClips = [
+    {
+      keyCode: 81,
+      keyTrigger: 'Q',
+      id: 'Chord-1',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+    },
+    {
+      keyCode: 87,
+      keyTrigger: 'W',
+      id: 'Chord-2',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+    },
+    {
+      keyCode: 69,
+      keyTrigger: 'E',
+      id: 'Chord-3',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+    },
+    {
+      keyCode: 65,
+      keyTrigger: 'A',
+      id: 'Shaker',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+    },
+    {
+      keyCode: 83,
+      keyTrigger: 'S',
+      id: 'Open-HH',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+    },
+    {
+      keyCode: 68,
+      keyTrigger: 'D',
+      id: 'Closed-HH',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+    },
+    {
+      keyCode: 90,
+      keyTrigger: 'Z',
+      id: 'Guitar-short',
+      src: './clip/clip1.wav'
+    },
+    {
+      keyCode: 88,
+      keyTrigger: 'X',
+      id: 'Guitar-short-2',
+      src: './clip/clip2.wav'
+    },
+    {
+      keyCode: 67,
+      keyTrigger: 'C',
+      id: 'Probably-Bass',
+      url: './clip/clip3.wav'
+    }
+  ];
 
-            this.state={
-                this: null
-            }
-        }
-
-
-        render(){
-            return (
-                <div class="main">
-                     {/* button */}
-                    <div class="buttons row">
-                        <div class="btn-box">
-                            <div class="col-3 drum-pad drum-pad btn btn-primary m-1 " id="drum-0">Q
-                            <audio src="./clip/" class="clip" id ="Q"></audio></div>
-                            <div class="col-3 drum-pad btn btn-dark m-1 " id="drum-1">W
-                            <audio src="./clip/" class="clip" id ="W"></audio></div>
-                            <div class="col-3 drum-pad btn btn-secondary m-1" id="drum-2">E
-                            <audio src="./clip/" class="clip" id ="E"></audio></div>
-                        </div>
-                        <div class="btn-box">
-                            <div class="col-3 drum-pad btn btn-info m-1" id="drum-3">A
-                            <audio src="./clip/" class="clip" id ="A"></audio></div>
-                            <div class="col-3 drum-pad btn btn-success m-1" id="drum-4">S
-                            <audio src="./clip/" class="clip" id ="S"></audio></div>
-                            <div class="col-3 drum-pad btn btn-primary m-1" id="drum-5">D
-                            <audio src="./clip/" class="clip" id ="D"></audio></div>
-                        </div>
-                        <div class="btn-box">
-                            <div class="col-3 drum-pad btn btn-danger m-1" id="drum-6">Z
-                            <audio src="./clip/" class="clip" id ="Z"></audio></div>
-                            <div class="col-3 drum-pad btn btn-dark m-1" id="drum-7">X
-                            <audio src="./clip/" class="clip" id ="X"></audio></div>
-                            <div class="col-3 drum-pad btn btn-warning m-1" id="drum-8">C
-                            <audio src="./clip/" class="clip" id ="C"></audio></div>
-                        </div>
-                    </div>
-                    {/* control */}
-                    <div class="row"></div>
-                </div>
-
-            )
-        }
-
+const App = () =>{
+    return (
+    <div className="min-vh-100 text-white d-flex align-items-center justify-content-center">
+        <div className="text-center col-7 bg-secondary">
+            <h2 className="text-white">HIT THE BUTTON</h2>
+            {audioClips.map((clip)=> (
+             <Pad key={clip.id} clip={clip}/>
+               ))}
+        </div>
+    </div> 
+    )
 }
+const Pad= ({clip})=>{
 
-ReactDOM.render(<App />, document.getElementById('app'))
+    const [active, setActive] = React.useState(false);
+
+    React.useEffect(()=>{
+        document.addEventListener('keydown', handleKeyPress);
+            return () =>{
+                document.removeEventListener('keydown', handleKeyPress);
+            }
+    }, []);
+    const handleKeyPress = (e) =>{
+        if(e.keyCode === clip.keyCode){
+            playit();
+        }
+    }
+
+    const playit = () =>{
+        let audio = document.getElementById(clip.keyTrigger);
+        setActive({
+            active:true
+        });
+        setTimeout(()=> setActive(false), 250);
+        audio.currentTime = 0;
+        audio.play();
+    };
+
+    return(
+        <div onClick = {playit} className={`btn btn-warning p-4 m-2 ${active && "btn-danger"}`}>
+            <audio className="clip" id={clip.keyTrigger} src={clip.url || clip.src} />
+            {clip.keyTrigger}
+        </div>
+    );
+}
+ReactDOM.render(<App/>, document.getElementById('app'));
