bc0c83164e4f8d3e36e067c8812594f9773c40aa
mmmG <a331998513@gmail.com>
2021-03-07 12:30:24 +0100

    frame without function

--- docs/calculator/calculator.js
+++ docs/calculator/calculator.js
@@ -1,25 +1,26 @@
 const App = ()=>{
+    const [output,setOutput] = React.useState("");
     return (
         <div className="container">
             <div className="bg-dark text-white grid-container">
-                <div className="button input">0123</div>
-                <div onClick={} className="button AC">AC</div>
-                <div onClick={} className="button symbol-color slash">/</div>
-                <div onClick={} className="button symbol-color times">X</div>
-                <div onClick={} className="button number-color seven">7</div>
-                <div onClick={} className="button number-color eight">8</div>
-                <div onClick={} className="button number-color nine">9</div>
-                <div onClick={} className="button symbol-color minus">-</div>
-                <div onClick={} className="button number-color four">4</div>
-                <div onClick={} className="button number-color five">5</div>
-                <div onClick={} className="button number-color siv">6</div>
-                <div onClick={} className="button symbol-color plus">+</div>
-                <div onClick={} className="button number-color one">1</div>
-                <div onClick={} className="button number-color two">2</div>
-                <div onClick={} className="button number-color three">3</div>
-                <div onClick={} className="button number-color zero">0</div>
-                <div onClick={} className="button number-color dot">.</div>
-                <div onClick={} className="button equal">=</div>
+                <div className="button output">0123</div>
+                <div onClick={output} className="button AC">AC</div>
+                <div onClick={output} className="button symbol-color slash">/</div>
+                <div onClick={output} className="button symbol-color times">X</div>
+                <div onClick={output} className="button number-color seven">7</div>
+                <div onClick={output} className="button number-color eight">8</div>
+                <div onClick={output} className="button number-color nine">9</div>
+                <div onClick={output} className="button symbol-color minus">-</div>
+                <div onClick={output} className="button number-color four">4</div>
+                <div onClick={output} className="button number-color five">5</div>
+                <div onClick={output} className="button number-color siv">6</div>
+                <div onClick={output} className="button symbol-color plus">+</div>
+                <div onClick={output} className="button number-color one">1</div>
+                <div onClick={output} className="button number-color two">2</div>
+                <div onClick={output} className="button number-color three">3</div>
+                <div onClick={output} className="button number-color zero">0</div>
+                <div onClick={output} className="button number-color dot">.</div>
+                <div onClick={output} className="button equal">=</div>
             </div>
         </div>
     )
