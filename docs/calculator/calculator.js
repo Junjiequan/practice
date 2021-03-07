0923528728990231ae0d9cf9a02e297654d0d617
mmmG <a331998513@gmail.com>
2021-03-07 12:26:38 +0100

    calculator frame without functions

--- docs/calculator/calculator.js
+++ docs/calculator/calculator.js
@@ -1,8 +1,25 @@
 const App = ()=>{
     return (
-        <div class="container">Hellow world
-            <div>
-                <div></div>
+        <div className="container">
+            <div className="bg-dark text-white grid-container">
+                <div className="button input">0123</div>
+                <div onClick={} className="button AC">AC</div>
+                <div onClick={} className="button symbol-color slash">/</div>
+                <div onClick={} className="button symbol-color times">X</div>
+                <div onClick={} className="button number-color seven">7</div>
+                <div onClick={} className="button number-color eight">8</div>
+                <div onClick={} className="button number-color nine">9</div>
+                <div onClick={} className="button symbol-color minus">-</div>
+                <div onClick={} className="button number-color four">4</div>
+                <div onClick={} className="button number-color five">5</div>
+                <div onClick={} className="button number-color siv">6</div>
+                <div onClick={} className="button symbol-color plus">+</div>
+                <div onClick={} className="button number-color one">1</div>
+                <div onClick={} className="button number-color two">2</div>
+                <div onClick={} className="button number-color three">3</div>
+                <div onClick={} className="button number-color zero">0</div>
+                <div onClick={} className="button number-color dot">.</div>
+                <div onClick={} className="button equal">=</div>
             </div>
         </div>
     )
