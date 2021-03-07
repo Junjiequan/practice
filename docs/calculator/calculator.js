ae20f9f10decc3aa28ce2a07d2dad14619c18696
mmmG <a331998513@gmail.com>
2021-03-07 11:16:37 +0100

    calculator 1%

---
+++ docs/calculator/calculator.js
@@ -0,0 +1,10 @@
+const App = ()=>{
+    return (
+        <div class="container">Hellow world
+            <div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+ReactDOM.render(<App/>, document.getElementById('app'))
