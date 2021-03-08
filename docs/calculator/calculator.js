8038720814c494561a7740c65d6db35ac1188fc1
mmmG <a331998513@gmail.com>
2021-03-08 08:12:26 +0100

    added some Chinese words

--- docs/calculator/calculator.js
+++ docs/calculator/calculator.js
@@ -22,7 +22,7 @@ const App = ()=>{
              }
              if(dotOnly.test(element)){
                 if (dotOnly.test(checkArr[checkArr.length-2])){
-                    alert("你见过0.0.6的数字吗")
+                    alert("别乱按，会弄坏计算器的")
                     setOutput('')
                 }
             }
