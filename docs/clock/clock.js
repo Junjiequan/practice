144791f662f223182b4e35384769c5d16d4ba199
mmmG <a331998513@gmail.com>
2021-03-09 04:55:11 +0100

    90%

--- docs/clock/clock.js
+++ docs/clock/clock.js
@@ -1,10 +1,20 @@
 const App = () =>{
-    const [sessiontime, setSessiontime] = React.useState(5);
-    const [breaklength, setBreaklength] = React.useState(5);
-    const [sessionlength, setSessionlength] = React.useState(5);
+    const [sessiontime, setSessiontime] = React.useState(25*60);
+    const [breaklength, setBreaklength] = React.useState(5*60);
+    const [sessionlength, setSessionlength] = React.useState(25*60);
     const [addtime, setAddtime] = React.useState(false);
     const [pause, setPause] = React.useState(false);
+    const [audio, setAudio] = React.useState(new Audio("./audio.wav"));
+    const [color,setColor] = React.useState({color:'white'});
 
+    const playAudio = ()=>{
+        audio.currentTime = 0;
+        audio.play();
+        // setTimeout(()=> {audio.pause()}, 2000);
+    }
+    const pauseaudio = ()=>{
+        audio.pause()
+    }
     const runTime = (time) =>{
         let minutes = Math.floor(time /60);
         let seconds = time % 60;
@@ -18,17 +28,31 @@ const App = () =>{
         let second = 1000;
         let date = new Date().getTime();
         let nextDate = new Date().getTime() + second;
+        let stop = pause;
         if (!addtime) {
             let interval = setInterval(()=>{
                 date = new Date().getTime();
                 if ( date > nextDate) {
                     setSessiontime(prev=>{
+                        if(prev <= 0 && !stop ){
+                            playAudio();
+                            stop = true;
+                            setPause(true);
+                            return breaklength
+                        } else if (prev <=0 && stop){
+                            playAudio();
+                            stop = false;
+                            setPause(false);
+                            return sessionlength
+                        } else if (prev <=60 ){
+                            setColor({color:'red'})
+                        } else if (prev > 60 ){
+                            setColor({color:'white'})
+                        }   
                     return prev - 1;
                     });
                     nextDate += second;
-                    console.log(nextDate)
-                } else if ( nextDate < 3 && data == 0){
-                    return ;
+
                 }
             }, 30);
             console.log(interval)
@@ -62,12 +86,15 @@ const App = () =>{
         setSessiontime(25*60);
         setBreaklength(5*60);
         setSessionlength(25*60);
+        setColor({color:'white'});
     }
     return (
-        <div className="container">
-            <div className="text-center">
-                <div ><h1>25 + 5 Clock</h1></div>
-                <div className="d-flex mt-2 ">
+        <div className="text-center d-flex justify-content-center align-items-center min-vh-100">
+            <div className="time-box shadow radius b">
+                <div className="pt-3">
+                    <h3>25+5 Clock</h3>
+                </div>
+                <div className="d-flex mt-2">
                     <Left 
                     title="Break length"
                     changeTime= {changeBreak}
@@ -81,11 +108,14 @@ const App = () =>{
                     runTime = {runTime}
                     />
                 </div>
-                <div className="session-time  mt-2">
-                    <h4>Session</h4>
-                    <p className="display-2">{runTime(sessiontime)}</p>
-                        <button onClick={startTime} className="btn btn-dark p-2 m-3 mt-0">{addtime ? <div>stop</div> : <div>play</div>}</button>
-                        <button onClick={reSet}className="btn btn-danger p-2 m-3 mt-0" >reset</button>
+                <div className=" mt-2 text-white d-flex justify-content-center " >
+                    <div className="bg-dark shadow session-time" >
+                    <h4 className="pt-3">{pause? "Break" : "Session"}</h4>
+                    <p className="display-2" style={color}>{runTime(sessiontime)}</p>
+                        <button onClick={startTime} className="click btn btn-light p-2 m-1" >{addtime ? <div>stop</div> : <div>play</div>}</button>
+                        <button onClick={reSet}className="click btn btn-danger p-2 m-1" >reset</button>
+                        <button onClick={pauseaudio}className="click btn btn-secondary p-2 m-1" >mute</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -95,9 +125,9 @@ const Left = ({title, changeTime,time,runTime})=>{
     return (
             <div className="m-4 text-center">
                 <h4>{title}</h4>
-                <button onClick={()=> changeTime(-60, time )} className="btn btn-primary p-4 m-3"></button>
-                <span className="h4">{runTime(time)}</span>
-                <button onClick={()=> changeTime(+60, time)} className="btn btn-danger p-4 m-3"></button>
+                <button onClick={()=> changeTime(-60, time )} className="btn btn-dark p-1 m-1"><i className="fa fa-arrow-down" aria-hidden="true"></i></button>
+                <span className="p-2 font">{runTime(time)}</span>
+                <button onClick={()=> changeTime(+60, time)} className="btn btn-dark p-1 m-1"><i className="fa fa-arrow-up" aria-hidden="true"></i></button>
             </div>
     )
 }
@@ -105,9 +135,9 @@ const Right = ({title, changeTime,time,runTime})=>{
     return (
             <div className="m-4 text-center">
                 <h4>{title}</h4>
-                <button onClick={()=> changeTime(-60, time )} className="btn btn-primary p-4 m-3"></button>
-                <span className="h4">{runTime(time)}</span>
-                <button onClick={()=> changeTime(+60, time)} className="btn btn-danger p-4 m-3"></button>
+                <button onClick={()=> changeTime(-60, time )} className="btn btn-dark p-1"><i className="fa fa-arrow-down" aria-hidden="true"></i></button>
+                <span className="p-2 font">{runTime(time)}</span>
+                <button onClick={()=> changeTime(+60, time)} className="btn btn-dark p-1"><i className="fa fa-arrow-up" aria-hidden="true"></i></button>
             </div>
     )
 }
