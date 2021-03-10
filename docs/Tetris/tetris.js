3773ba41fbf7cc7aa677e469c48ce5fd44b0ab2c
mmmG <a331998513@gmail.com>
2021-03-10 08:05:31 +0100

    5%

--- docs/Tetris/tetris.js
+++ docs/Tetris/tetris.js
@@ -1,19 +1,57 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-const grid = document.querySelector('.grid');
-let square = document.querySelectorAll('.grid div');
-const scoredisplay = document.querySelector('#score');
-const startbtn = document.querySelector('#start-btn');
+    //Tetromino shape //5 types->[L,Z,T,O,I]
 const width = 10;
+const lTetromino = [
+    [1, width+1, width*2+1, 2],                 //rotation1
+    [width, width+1, width+2, width*2+2],       //rotation2
+    [1, width+1, width*2+1, width*2],           //ratation3
+    [width, width*2, width*2+1,width*2+2],      //rotation4
+];
+const zTetromino = [
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
+];
+const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+];
+const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+];
+const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+]
+    
+const grid = document.querySelector('.grid');
+var squares = document.querySelectorAll('.grid div');
+const Scoredisplay = document.querySelector('#score');
+const Startbtn = document.querySelector('#start-btn');
 
+const theTetrominos = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
+let currentPosition = 4
+let current = theTetrominos[0][0];
 
+//first rotation in the first tetromino
+const draw = () =>{
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add("tetromino")
+    })
+}
+draw();
 
-
-
-
-
-
-
+grid.classList.add("tetromino")
+console.log(current)
 
 
 
