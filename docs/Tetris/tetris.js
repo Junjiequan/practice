e50ffde44af51e2532fc86329733172c29ae3b96
mmmG <a331998513@gmail.com>
2021-03-10 06:41:03 +0100

    1%

---
+++ docs/Tetris/tetris.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+const grid = document.querySelector('.grid');
+let square = document.querySelectorAll('.grid div');
+const scoredisplay = document.querySelector('#score');
+const startbtn = document.querySelector('#start-btn');
+const width = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
